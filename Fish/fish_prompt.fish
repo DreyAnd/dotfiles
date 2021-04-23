aff2aa3933a5c956f938d89e15ef7a98f905ae9f
DreyAnd <58053372+DreyAnd@users.noreply.github.com>
2021-04-23 14:44:10 +0200

    Update fish_prompt.fish

--- Fish/fish_prompt.fish
+++ Fish/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt
     printf '\033[1;31m%s\033[00m' (hostname)
-    printf '\e[96m ⚏ \033[1;31m{\033[00m \e[35m%s \033[1;31m}\e[96m ─╼ \033[00m ' $PWD
+    printf '\e[96m ⚏ \033[1;31m{\033[00m \e[35m%s \033[1;31m}\e[96m ─╼ \033[00m ' (pwd | sed -E "s-^$HOME(\$|(/.*))-~\2-")
 end
